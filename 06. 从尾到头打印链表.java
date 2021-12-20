5d03feb27577cb3190d559bfdece0b94396224e7
Fred-Gu <31855923+Fred-Gu@users.noreply.github.com>
2021-12-20 16:59:24 -0500

    Create 06. 从尾到头打印链表.java

---
+++ 06. 从尾到头打印链表.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        if (head == null) {
+            return new int[0];
+        }
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] result = new int[stack.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
