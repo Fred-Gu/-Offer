7eacc5c41294ca3285eb5147e9591dad95fdbe76
Fred-Gu <31855923+Fred-Gu@users.noreply.github.com>
2021-12-23 15:02:14 -0500

    Create 24. 反转链表.java

---
+++ 24. 反转链表.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+}
