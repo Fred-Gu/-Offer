9f64f3abefa0ce0157c0d587009dd2ad116fde61
Fred-Gu <31855923+Fred-Gu@users.noreply.github.com>
2021-12-27 15:13:05 -0500

    Create 32 - II. 从上到下打印二叉树 II.java

---
+++ 32 - II. 从上到下打印二叉树 II.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root != null) queue.offer(root);
+        while(!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+}
